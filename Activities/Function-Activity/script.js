f8ab3d242122b150cb6603c98457aba80beffb38
cbhots <ajcusay@filamer.edu.ph>
2025-03-25 23:34:31 +0800

    Activities

---
+++ Activities/Function-Activity/script.js
@@ -0,0 +1,15 @@
+function whatShallIWear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } 
+    else {
+        console.log("Wear a t-shirt");
+    }
+
+    if (temp < 70) {
+        console.log("Wear a sweater");
+    }
+}
+whatShallIWear(80);
+whatShallIWear(60);
+whatShallIWear(50);
